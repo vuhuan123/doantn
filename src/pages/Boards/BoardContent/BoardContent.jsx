55ef1615c55cc49ea6e36df7d9a931b14ee409d3
Vupham <phamvanv15@gmail.com>
2025-03-21 20:43:48 +0700

    handleDragStart

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,10 +1,16 @@
 import { Box, Tooltip, Typography } from "@mui/material";
 import ListColumms from "./ListColumm/ListColumms";
 import { mapOrder } from "~/utils/sort";
-import { DndContext, PointerSensor, useSensor, useSensors, MouseSensor, TouchSensor } from "@dnd-kit/core";
+import { DndContext, DragOverlay,PointerSensor, useSensor, useSensors, MouseSensor, TouchSensor, defaultDropAnimationSideEffects } from "@dnd-kit/core";
 import { useEffect, useState } from "react";
 import { arrayMove } from "@dnd-kit/sortable";
+import Column from "./ListColumm/Columm/Column";
+import Card from "./ListColumm/Columm/ListCards/Card/Card";
 
+const ACTIVE_DRAG_ITEM_TYPE = {
+    COLUMN: 'ACTIVE_DRAG_ITEM_TYPE_COLLUM',
+    CARD: 'ACTIVE_DRAG_ITEM_TYPE_CARD',
+}
 function BoardContent({board}) {
     // yeeu cau duy chuyen 10px khi hold
    // const pointerSensor = useSensor(PointerSensor, {activationConstraint:{distance: 10}})
@@ -16,10 +22,23 @@ function BoardContent({board}) {
     const sensors = useSensors(mouseSensors, touchSensor)
     const [oderedColumnsState, setOderedColumnsState] = useState([])
 
+    //cung mot thoi diem chi co mot phan tu dang ddc keo(column or card)
+    const [activeDragItemId, setActiveDragItemId] = useState(null)
+    const [activeDragItemType, setActiveDragItemType] = useState(null)
+    const [activeDragItemData, setActiveDragItemData] = useState(null)
     useEffect( ()=>{
         const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
         setOderedColumnsState(orderedColumns)
     }, [board])
+
+    //khi bat dau keo 1 phan tu(drag)
+    const handleDragStart = (event)=>{
+        console.log('handle Start:',  event);
+        setActiveDragItemId(event?.active.id)
+        setActiveDragItemType(event?.active?.data?.current?.columnId ? ACTIVE_DRAG_ITEM_TYPE.CARD : ACTIVE_DRAG_ITEM_TYPE.COLUMN)
+        setActiveDragItemData(event?.active?.data?.current)
+    }
+    //khi ket thuc keo 1 phan tu (drop)
     const handleDragEnd = (event)=>{
         console.log('handleDragEnd: ', event);
         const {active, over} = event
@@ -31,9 +50,27 @@ function BoardContent({board}) {
             const newIndex = oderedColumnsState.findIndex(c => c._id === over.id)
             setOderedColumnsState( arrayMove(oderedColumnsState, oldIndex, newIndex))
         }
+        // keo ms co data, k keo thi set null
+        setActiveDragItemId(null)
+        setActiveDragItemType(null)
+        setActiveDragItemData(null)
     }
+
+  const dropAnimation = {
+    sideEffects : defaultDropAnimationSideEffects({
+        styles:{
+            active:{
+                opacity: '0.5'
+            }
+        }
+    })
+  }
+    
     return (
-    <DndContext onDragEnd={handleDragEnd} sensors={sensors} >
+    <DndContext 
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd} 
+        sensors={sensors} >
         <Box sx={{
             bgcolor: (theme) => theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
             width: '100%',
@@ -41,6 +78,11 @@ function BoardContent({board}) {
             height: (theme) => theme.trello.BoardContentHeight,
         }}>
             <ListColumms columns = {oderedColumnsState} />
+            <DragOverlay dropAnimation={dropAnimation}>
+                {(!activeDragItemType && null)}
+                {( activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) && <Column column={activeDragItemData} />}
+                {( activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD) && <Card card={activeDragItemData} />}
+            </DragOverlay>
         </Box>
     </DndContext>
 
--- src/pages/Boards/BoardContent/ListColumm/Columm/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumm/Columm/ListCards/Card/Card.jsx
@@ -8,12 +8,29 @@ import CardMedia from '@mui/material/CardMedia';
 import GroupIcon from '@mui/icons-material/Group';
 import CommentIcon from '@mui/icons-material/Comment';
 import AttachmentIcon from '@mui/icons-material/Attachment';
-
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 function Card({ card }) {
-    
+    const { attributes, listeners, setNodeRef, transform, transition, isDragging} = useSortable({
+            id: card._id,
+            data: { ...card }
+        });
+        const dndKitCardStyles = {
+            touchAction: 'none',
+            transform: CSS.Translate.toString(transform),
+            transition,
+            opacity: isDragging ? 0.7 : undefined
+        };
     return (
+        <div>
+         
         <Box>
-            <MuiCard sx={{
+            <MuiCard 
+             ref ={setNodeRef} 
+             style = {dndKitCardStyles}
+             {...attributes}
+             {...listeners}
+            sx={{
                 cursor: 'pointer',
                 boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
                 overflow: 'unset'
@@ -49,7 +66,8 @@ function Card({ card }) {
                 </CardContent>
             </MuiCard> */}
         </Box>
-
+   
+        </div>
     )
 }
 
