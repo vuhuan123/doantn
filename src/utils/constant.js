dd3bd1dcde6a3ff31ed5a103e0b5c4f8733edfb4
Vupham <phamvanv15@gmail.com>
2025-06-07 17:07:57 +0700

    fix BUILD MODE to deploy

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,13 @@
-// export const API_ROOT = 'http://localhost:5175'
-export const API_ROOT = 'https://planify-api.onrender.com'
+import.meta.env
+let apiRoot = ''
+console.log('meta', import.meta.env)
+
+if (import.meta.env.DEV) {
+    apiRoot = 'http://localhost:5175'
+}else {
+    apiRoot = 'https://planify-api.onrender.com'
+}
+export const API_ROOT = apiRoot
 
 export const DEFAULT_PAGE = 1
 export const DEFAULT_ITEMS_PER_PAGE = 12
